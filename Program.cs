ba632269bc0a1f49c43291b18f8b1f0e3e72014f
Caio <caio_pouzada@estudante.sesisenai.org.br>
2026-06-11 20:40:01 -0300

    Questão 1

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Escreva sua idade: ");
+int idade = int.Parse(Console.ReadLine()!);
+
+if (idade >= 16) {
+    Console.WriteLine("Entrada liberada.");
+} else {
+    Console.WriteLine("Entrada permitida somente com um responsável.");
+}
